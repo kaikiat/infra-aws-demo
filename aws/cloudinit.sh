5ff2c43b935973ac235eed23ffa57ef870cb766e
kaikiat <pohkaikiat98@gmail.com>
2023-08-08 00:31:22 +0800

    update

--- aws/cloudinit.sh
+++ aws/cloudinit.sh
@@ -3,7 +3,6 @@ sudo apt-get install -y git
 sudo apt-get install -y python3-pip
 sudo apt-get install -y gunicorn
 sudo apt install -y python3-flask
-sudo apt install -y python3.10-venv
 
 git config --global user.name "kaikiat"
 git config --global user.email "pohkaikiat98@gmail.com"
@@ -34,5 +33,28 @@ sudo systemctl daemon-reload
 sudo systemctl start server
 sudo systemctl enable server
 
+sudo apt install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+cat << EOF | sudo tee /etc/nginx/sites-available/default
+server {
+    listen 80;
+    server_name localhost;
+
+    location / {
+        proxy_pass http://localhost:5000;
+    }
+}
+EOF
+# Not required
+# sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+# For fun
+sudo nginx -t
+sudo systemctl reload nginx
+
+
+
+# https://medium.com/techfront/step-by-step-visual-guide-on-deploying-a-flask-application-on-aws-ec2-8e3e8b82c4f7
 # cat /var/log/cloud-init-output.log
 # cat /var/log/cloud-init.log
