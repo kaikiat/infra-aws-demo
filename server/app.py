bd18cea926b6fb2023494115752c35ede92ab905
kaikiat <pohkaikiat98@gmail.com>
2023-08-09 01:06:24 +0800

    fix app.py

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
+from flask import request
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World !!'
+	hostname = request.headers.get('Host')
+	return f'{str(hostname)}'
 
 
 @app.route('/healthz')
