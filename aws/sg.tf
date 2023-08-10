5144a11ac0127aab7bb58c7b41ef84a232419ed8
I589811 <kai.kiat.poh@sap.com>
2023-08-10 21:47:54 +0800

    update sg

--- aws/sg.tf
+++ aws/sg.tf
@@ -41,21 +41,14 @@ resource "aws_security_group" "gt_ec2_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.gt_alb_sg.id]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.gt_alb_sg.id]
   }
 
   ingress {
